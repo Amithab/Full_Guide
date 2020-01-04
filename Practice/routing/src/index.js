cae738a25515357d887c8b5b1a3f5854b266b194
Amithab <amiquest12@gmail.com>
2020-01-03 16:24:15 -0800

    adding links and routing in practice

--- Practice/routing/src/index.js
+++ Practice/routing/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import Users from './users';
+import Contact from './contact';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} /> {/* use exact or / is shared by both users and contact and home will appear in both*/}
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
